7cfdfefbd23f3f0d287898750212e7fe202906f0
miranopari <94349275+miranopari@users.noreply.github.com>
2022-01-10 00:09:20 +0900

    Update kawauso021.py

--- kawauso021.py
+++ kawauso021.py
@@ -130,8 +130,10 @@ def game(player1, player2):
 
 # AI 用のインターフェース
   
+YUSEN=[0,5,30,35,2,3,17,23,12,18,32,33,9,16,8,15,22,14,21,13,20,27,19,26,4,11,1,29,6,34,24,31,10,7,28,25]
 def my_AI(board, color): #おチビちゃんAI
-  for position in range(N*N):
+  for i in range(N*N):
+    position=YUSEN[i]
     if put_and_reverse(board, position, color):
       return position
   return 0
